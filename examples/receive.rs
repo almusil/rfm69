04b34a7d0037965269f1e43e68dd781975278246
Ales Musil <amusil@redhat.com>
2021-10-06 08:53:40 +0200

    Update examples to encourage more idiomatic code
    - Replace all unwraps with ? using anyhow
    - Create utilities library that should house common code
    for tests
    - Move Packet to utilities to reduce duplication

--- examples/receive.rs
+++ examples/receive.rs
@@ -1,42 +1,43 @@
-extern crate linux_embedded_hal;
-extern crate rfm69;
-
+use anyhow::Result;
 use linux_embedded_hal::spidev::{SpiModeFlags, SpidevOptions};
 use linux_embedded_hal::sysfs_gpio::Direction;
 use linux_embedded_hal::{Delay, Spidev, SysfsPin};
 use rfm69::Rfm69;
+use utilities::rfm_error;
 
-fn main() {
+fn main() -> Result<()> {
     // Configure CS pin
     let cs = SysfsPin::new(25);
-    cs.export().unwrap();
-    cs.set_direction(Direction::High).unwrap();
+    cs.export()?;
+    cs.set_direction(Direction::High)?;
 
     // Configure SPI 8 bits, Mode 0
-    let mut spi = Spidev::open("/dev/spidev0.0").unwrap();
+    let mut spi = Spidev::open("/dev/spidev0.0")?;
     let options = SpidevOptions::new()
         .bits_per_word(8)
         .max_speed_hz(1_000_000)
         .mode(SpiModeFlags::SPI_MODE_0)
         .build();
-    spi.configure(&options).unwrap();
+    spi.configure(&options)?;
 
     // Create rfm struct with defaults that are set after reset
     let mut rfm = Rfm69::new(spi, cs, Delay);
 
     // Print content of all RFM registers
-    for (index, val) in rfm.read_all_regs().ok().unwrap().iter().enumerate() {
+    for (index, val) in rfm_error!(rfm.read_all_regs())?.iter().enumerate() {
         println!("Register 0x{:02x} = 0x{:02x}", index + 1, val);
     }
 
     // Prepare buffer to store the received data
     let mut buffer = [0; 64];
-    rfm.recv(&mut buffer).ok().unwrap();
+    rfm_error!(rfm.recv(&mut buffer))?;
     // Print received data
     for (index, val) in buffer.iter().enumerate() {
         println!("Value at {} = {}", index, val)
     }
 
     // Un-export the CS pin
-    SysfsPin::new(25).unexport().unwrap();
+    SysfsPin::new(25).unexport()?;
+
+    Ok(())
 }
